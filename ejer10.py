504e866b37dc6f394cc00528effa8852c3323b33
ALP300 <aitorpalomino1@gmail.com>
2025-07-20 21:26:29 -0500

    A

---
+++ ejer10.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+datos = pd.read_csv('datosyt.csv')
+print(datos.dtypes)
+print(datos.sort_values(by='xs', ascending=True))
+df1= pd.DataFrame(np.sort(datos.values, axis=0),index=datos.index, columns=datos.columns)
+print(df1)
